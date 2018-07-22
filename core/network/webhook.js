24eb9887e0ff46ee3e017e5a7e099926673d6443
AjK <alberto.garbui@gmail.com>
2018-07-22 10:48:49 +0200

    [Fix] Webhook use req.body rather than parse raw body

--- core/network/webhook.js
+++ core/network/webhook.js
@@ -10,30 +10,17 @@ module.exports = function (hookPath, updateHandler, errorHandler) {
       res.statusCode = 403
       return res.end()
     }
-    let body = ''
-    req.on('data', (chunk) => {
-      body += chunk.toString()
-    })
-    req.on('end', () => {
-      let update = {}
-      try {
-        update = JSON.parse(body)
-      } catch (error) {
-        res.writeHead(415)
-        res.end()
-        return errorHandler(error)
-      }
-      updateHandler(update, res)
-        .then(() => {
-          if (!res.finished) {
-            res.end()
-          }
-        })
-        .catch((err) => {
-          debug('Webhook error', err)
-          res.writeHead(500)
+    const update = req.body
+    updateHandler(update, res)
+      .then(() => {
+        if(!res.finished) {
           res.end()
-        })
-    })
+        }
+      })
+      .catch(err => {
+        debug('Webhook error', err)
+        res.writeHead(500)
+        res.end()
+      })
   }
 }
